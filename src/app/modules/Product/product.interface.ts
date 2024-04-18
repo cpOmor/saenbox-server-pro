947b3d10024fa03746b6c828ea7068e1925e97b6
OmarWebDev <gteam9035@gmail.com>
2024-04-19 03:00:36 +0600

    product

--- src/app/modules/Product/product.interface.ts
+++ src/app/modules/Product/product.interface.ts
@@ -1,6 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Types } from 'mongoose';
+
+export type TAttribute = {
+  attribute: [{ name: string; title: string[] }];
+};
+
+export type TRatings = {
+  ratings: [{ user: Types.ObjectId; rating: string; title: string }];
+};
 
 export type TProduct = {
   seller: Types.ObjectId;
   title: string;
+  salesPrice: string;
+  regularPrice: string;
+  shippingConst: string;
+  quantity: number;
+  attribute: TAttribute;
+  category: string;
+  description: string;
+  ratings: TRatings;
+  tag: string[]; 
 };
--- src/app/modules/Product/product.model.ts
+++ src/app/modules/Product/product.model.ts
@@ -1,5 +1,36 @@
 import { Schema, model } from 'mongoose';
-import { TProduct } from './product.interface';
+import { TAttribute, TProduct, TRatings } from './product.interface';
+
+const RatingSchema = new Schema<TRatings>({
+  ratings: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+      },
+      rating: {
+        type: String,
+        required: [true, 'Rating is required'],
+      },
+      title: {
+        type: String,
+        required: [true, 'Title is required'],
+      },
+    },
+  ],
+});
+
+const AttributeSchema = new Schema<TAttribute>({
+  attribute: [
+    {
+      name: {
+        type: String,
+        required: [true, 'Name is required'],
+      },
+
+      title: { type: [String], required: [true, 'Title is required'] },
+    },
+  ],
+});
 
 const productSchema = new Schema<TProduct>({
   seller: {
@@ -10,6 +41,37 @@ const productSchema = new Schema<TProduct>({
     type: String,
     required: [true, 'Title is required'],
   },
+  salesPrice: {
+    type: String,
+    required: [true, 'Sales price is required'],
+  },
+  regularPrice: {
+    type: String,
+  },
+  shippingConst: {
+    type: String,
+    required: [true, 'Shipping const price is required'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Quantity  is required'],
+  },
+  category: {
+    type: String,
+    required: [true, 'Category  is required'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Description  is required'],
+  },
+
+  ratings: RatingSchema,
+  tag: {
+    type: [String],
+    required: [true, 'Please add tags '],
+  },
+
+  attribute: AttributeSchema,
 });
 
 export const ProductModel = model<TProduct>('products', productSchema);
